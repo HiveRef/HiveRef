d1c866292475d226c731a81e3f73559e57057e60
Carlos Eduardo Goulart <142110393+CarlosEGoulart@users.noreply.github.com>
2026-08-11 14:30:29 +0000

    test: remove unused DB import in RLS migration test

--- tests/Feature/Migrations/RLSMigrationTest.php
+++ tests/Feature/Migrations/RLSMigrationTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
 
 test('RLS migration creates policies on PostgreSQL', function () {
     // Only run if PostgreSQL is available
